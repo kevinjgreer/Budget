08876ccff51d2bdef00fd2e396edc5e6a2a58b0d
Kevin Greer <kevin.greer@exp.com>
2020-02-26 20:20:50 -0500

    updates

--- Public/Convert-CSVAdditionFinancial.ps1
+++ Public/Convert-CSVAdditionFinancial.ps1
@@ -3,7 +3,13 @@ function Convert-CSVAdditionFinancial {
     param (
         #validate
         [Parameter()]
-        [String]$Path
+        [String]$Path,
+
+        [Parameter()]
+        [String]$BudgetPath = 'C:\Users\kgreer\Dropbox\Budget',
+
+        [Parameter()]
+        [String]$Account = 'AdditionFinancial_47'
     )
 
     begin {
@@ -16,9 +22,15 @@ function Convert-CSVAdditionFinancial {
         }
         $Content = "Date,1,Amount,Description,2,3,4,5,6,7,8,9,10`n" + (get-content -Path $Path -raw)
         $Content | Set-Content -Path $Path -Force
-        Import-Csv -Path $Path | Select-Object @{n = 'Date'; e = { ($_.Date).split(' ')[0] } }, Amount, Description | Export-Csv -Path "$(Split-Path -Path $Path -Parent)\ReadyforYNAB.csv" -NoTypeInformation -Force
+        $CSVData = Import-Csv -Path $Path | Select-Object @{n = 'Date'; e = { ($_.Date).split(' ')[0] } }, Amount, Description
+        $i = 0
+        ForEach ($item in $CSVData) {
+            Add-Member -InputObject $item -MemberType NoteProperty -Name 'number' -Value $i
+            $i++
+        }
+
+        $CSVData | Export-Csv -Path "$BudgetPath\Accounts\$Account\$(get-date -Format filedatetime)_$Account.csv" -NoTypeInformation -Force
         Remove-item -Path $Path
-        Rename-Item -Path "$(Split-Path -Path $Path -Parent)\ReadyforYNAB.csv" -NewName "$(Split-Path -Path $Path -Leaf)"
     }
 
     end {
---
+++ Public/Get-PaidBills.ps1
@@ -0,0 +1,58 @@
+function Get-PaidBills {
+    [CmdletBinding()]
+    param (
+
+        [Parameter()]
+        [String]$Path = "C:\users\kgreer\dropbox\budget\Bills.json",
+
+        [Parameter()]
+        [String]$Account = 'C:\users\kgreer\Dropbox\budget\Accounts\AdditionFinancial_47',
+
+        [Parameter()]
+        [DateTime]$Date = "$((Get-Date).month)/1/$((Get-Date).Year)"
+
+    )
+
+    begin {
+    }
+
+    process {
+
+        $Bills = ConvertFrom-Json -InputObject (Get-Content -Path $Path -raw)
+
+        $LedgerCSV = Get-ChildItem -Path $Account -filter '*.csv' | Sort-Object -Descending | Select-Object -first 1
+        $Ledger = Import-Csv -Path $LedgerCSV.FullName | Where-Object { (Get-Date $_.Date) -GE (Get-Date $Date) }
+
+        foreach ($Bill in $Bills) {
+            $FoundBill = $False
+            foreach ($Entry in $Ledger) {
+                if ($Entry.Description -match $Bill.Description) {
+                    $FoundBill = $true
+
+                    $Description = $Entry.Description
+                    $Amount = $Entry.Amount
+                    $BillDate = $Entry.Date
+                    [PSCustomObject]@{
+                        Bill        = $Bill.Bill
+                        Description = $Description
+                        Amount      = $Amount
+                        Date        = $BillDate
+                    }
+                    break
+                }
+
+
+            }
+            if ($FoundBill -eq $False) {
+                [PSCustomObject]@{
+                    Bill        = $Bill.Bill
+                    Description = $Null
+                    Amount      = $Null
+                    Date        = $Null
+                }
+            }
+        }
+    }
+    End { }
+
+}
